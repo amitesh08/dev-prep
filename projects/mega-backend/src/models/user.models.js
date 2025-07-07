0d28df75e4dedd5b5e4a9674051c4c8c32ca70f3
amitesh08 <aamitesh125@gmail.com>
2025-07-07 13:40:52 +0530

    change in schema

--- projects/mega-backend/src/models/user.models.js
+++ projects/mega-backend/src/models/user.models.js
@@ -2,6 +2,7 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
+
 const userSchema = new Schema(
   {
     avatar: {
