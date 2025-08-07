e07375ccb684c9a3ee6aa2b0cf4eec94fdd20547
amitesh08 <aamitesh125@gmail.com>
2025-08-07 21:45:27 +0530

    00-nextjs

---
+++ code/Nextjs/00-nextjs/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans flex justify-center items-center ">
+      <h1 className=" ">lets get started</h1>
+    </div>
+  );
+}
